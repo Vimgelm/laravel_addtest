2c9135769ec3a81eb48121fe07e233328ea84f5a
Vimgelm <vimanonim@gmail.com>
2022-02-03 01:45:09 +0300

    add breeze inertia and auth

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::get('test',function (){
+    return view('hello');
+});
