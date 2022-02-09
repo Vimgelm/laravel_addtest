e2572569912c093a12b0766cf9441eb15eb4d5c9
vim <vimanonim@gmail.com>
2022-02-09 13:41:32 +0300

    save in db all data questions without checkboxs

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Test;
+use App\Models\Question;
 
 class TestController extends Controller
 {
@@ -12,10 +13,27 @@ class TestController extends Controller
     }
 
     public function Result(Request $request){
-        $tests = New Test();
-        $tests-> name = $request->name;
-        $tests-> description = $request->description;
-        $tests-> owner = $request->owner;
-        $tests->save();
+        $test = New Test();
+        $questions = New Question();
+        $test->name = $request->name;
+        $test->description = $request->description;
+        $test->owner = $request->owner;
+        $test->save();
+        $id = $test->id;
+
+
+        foreach ($request->all() as $item) {
+            if(isset($item['id'])){
+                $questions->test_id = $id;
+                $questions->question = $item['question'];
+                $questions->answer1 = $item['answer1'];
+                $questions->answer2 = $item['answer2'];
+                $questions->answer3 = $item['answer3'];
+                $questions->answer4 = $item['answer4'];
+                $questions->save();
+            }
+
+
+        }
     }
 }
